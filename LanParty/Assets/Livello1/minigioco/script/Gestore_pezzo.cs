0ad6fb0658c0d2f07dbba745d9abb10ca4340aba
Daniele Villa <daniele.villa@marconirovereto.it>
2019-12-30 22:35:59 +0100

    introduzione prefab minigioco tubi

---
+++ LanParty/Assets/Livello1/minigioco/script/Gestore_pezzo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gestore_pezzo : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        transform.Rotate(0, 0, 90);
+    }
+}
