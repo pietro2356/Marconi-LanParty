482e4b8d711d4186f56505019745cc877670e3b7
pietro.rocchio <pietro.rocchio@marconirovereto.it>
2019-12-30 23:00:10 +0100

    Implementata griglia di gestione minigioco

---
+++ LanParty/Assets/Livello1/minigioco/script/GesgtioneGriglia.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GesgtioneGriglia : MonoBehaviour
+{
+    public Tubi[,] griglia = new Tubi[11, 6];
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public bool controllaPercorso()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ LanParty/Assets/Livello1/minigioco/script/Tubi.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Drawing;
+
+public enum tipoTubo
+{
+    DRITTO, CURVA
+}
+
+public class Tubi : MonoBehaviour
+{
+    public int x;
+    public int y;
+    public float gradi;
+    public tipoTubo tipoTubo;
+
+    public Tubi(int x, int y, float gradi, tipoTubo tipoTubo)
+    {
+        this.x = x;
+        this.y = y;
+        this.gradi = gradi;
+        this.tipoTubo = tipoTubo;
+    }
+}
