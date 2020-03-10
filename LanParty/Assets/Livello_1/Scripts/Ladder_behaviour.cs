96b8a6f7a35f80e9fea9d4efa4a3ac884fec85c8
Marco <marco.zani@marconirovereto.it>
2020-03-10 17:34:52 +0100

    fixes

--- LanParty/Assets/Livello_1/Scripts/Ladder_behaviour.cs
+++ LanParty/Assets/Livello_1/Scripts/Ladder_behaviour.cs
@@ -15,7 +15,7 @@ public class Ladder_behaviour : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.name == "Player")
+        if (other.tag == "Player")
         {
             player.onLadder = true;
             Debug.Log("test");
@@ -24,7 +24,7 @@ public class Ladder_behaviour : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.name == "Player")
+        if (other.tag == "Player")
             player.onLadder = false;
     }
 }
