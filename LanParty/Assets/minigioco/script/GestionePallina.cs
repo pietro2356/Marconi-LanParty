d80b7e20fcfab5259995bdb0a6e720e97ecd9c48
Daniele Villa <daniele.villa@marconirovereto.it>
2020-03-14 12:55:05 +0100

    inizio gioco palline

---
+++ LanParty/Assets/minigioco/script/GestionePallina.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestionePallina : MonoBehaviour
+{
+    public Tubi questo;
+
+    void OnMouseDown()
+    {
+        if (true)//GestioneGriglia.istanza.giocoAttivo)
+        {
+            Seleziona();
+            GestioneGriglia.istanza.ControllaVincita();
+        }
+    }
+
+    private void Seleziona()
+    {
+        Vector3 posizione = transform.position;
+        posizione.y = 2.5f;
+        transform.position = posizione;
+    }
+}
